a5b35cd0ce57ac4f61b5de80734bd2d4345f1140
Brian Ouellette <btouellette@gmail.com>
2025-03-13 01:35:47 +0000

    update typing

--- src/scripts/types.ts
+++ src/scripts/types.ts
@@ -1,55 +1,59 @@
 // Represents a movement applied to a slab from its current position (including the shelf) to the target area
-export interface MoveTransformation {
+export type MoveTransformation = {
     type: 'move';
     x: number; // These are from the center of the target area
     y: number;
 }
 
 // Represents a rotation applied to a slab
-export interface RotateTransformation {
+export type RotateTransformation = {
     type: 'rotate';
     rotation: number; // in degrees, about the center of the slab
 }
 
 // Represents a cut applied to a slab
 // A cut will always be along all intersections between the current slab position and the target area
-export interface CutTransformation {
+export type CutTransformation = {
     type: 'cut';
     resultingSlabs: Slab[];
 }
 
 // Represents a removal of a slab back to the shelf
-export interface RemoveTransformation {
+export type RemoveTransformation = {
     type: 'remove';
 }
 
 // Union type for all possible transformations
 export type Transformation = MoveTransformation | RotateTransformation | CutTransformation | RemoveTransformation;
 
-// Represents a slab of wood, current x/y/rotation are derived ffom the transformations
-export interface Slab {
+export type ShelvedSlab = {
     id: string;
     dataUrl: string; // Locally encoded base64 image
     width: number;
     height: number;
+}
+
+
+// Represents a slab of wood, current x/y/rotation are derived ffom the transformations
+export type Slab = ShelvedSlab & {
     cutSlabs: Slab[];
 }
 
 // Represents the target area for the final product
-export interface TargetArea {
+export type TargetArea = {
     type: 'rectangle' | 'ellipse';
     width: number;
     height: number;
 }
 
 // Represents a user command for undo/redo functionality
-export interface Command {
+export type Command = {
     slab: Slab;
     transformation: Transformation;
 }
 
 // Represents a project containing multiple slabs and a target area
-export interface Project {
+export type Project = {
     name: string;
     slabs: Slab[];
     targetArea: TargetArea;
@@ -57,6 +61,6 @@ export interface Project {
 }
 
 // Represents a list of projects, this is what is serialized to localStorage
-export interface ProjectList {
+export type ProjectList = {
     projects: Project[];
 }
